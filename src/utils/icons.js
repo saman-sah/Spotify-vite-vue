606352134de6759e75fb65ecc9decce10cedf5b5
saman-sah <sahraei.saman91@gmail.com>
2024-04-13 15:52:35 +0330

    complete library view

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,23 +1,33 @@
 import Plus from 'vue-material-design-icons/Plus.vue'
+import Play from 'vue-material-design-icons/Play.vue'
+import Pause from 'vue-material-design-icons/Pause.vue'
+import Heart from 'vue-material-design-icons/Heart.vue'
 import Magnify from 'vue-material-design-icons/Magnify.vue'
+import ChevronUp from 'vue-material-design-icons/ChevronUp.vue'
 import Television from 'vue-material-design-icons/Television.vue'
 import TrendingUp from 'vue-material-design-icons/TrendingUp.vue'
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue'
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
 import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
 import MovieOutline from 'vue-material-design-icons/MovieOutline.vue'
-import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
 import ChevronRight from 'vue-material-design-icons/ChevronRight.vue'
-import ChevronDown from 'vue-material-design-icons/ChevronDown.vue'
-import ChevronUp from 'vue-material-design-icons/ChevronUp.vue'
+import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue'
+import ClockTimeThreeOutline from 'vue-material-design-icons/ClockTimeThreeOutline.vue'
 
 export {
   Plus,
+  Play,
+  Heart,
+  Pause,
   Magnify,
+  ChevronUp,
   Television,
   TrendingUp,
   HomeOutline,
-  MovieOutline,
   ChevronLeft,
-  ChevronRight,
   ChevronDown,
-  ChevronUp
+  MovieOutline,
+  ChevronRight,
+  DotsHorizontal,
+  ClockTimeThreeOutline
 }
--- src/views/LibraryView.vue
+++ src/views/LibraryView.vue
@@ -1,8 +1,102 @@
 <script setup>
+import {
+  Play,
+  Pause,
+  Heart,
+  DotsHorizontal,
+  ClockTimeThreeOutline
+} from '../utils/icons.js'
+import artist from '../utils/artist.json'
 
+import SongRow from '../components/Library-View/SongRow.vue'
 </script>
 <template>
-  <div class="text-lg text-black">
-    Library View
+  <div class="p-8 overflow-hidden">
+    <button
+      type="button"
+      class="text-[33px] text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      {{ artist.name }}
+    </button>
+    <div class="py-1.5" />
+
+    <div class="flex items-center w-full relative h-full">
+      <img
+        :src="artist.albumCover"
+        alt=""
+        width="140"
+      >
+      <div class="w-full ml-5">
+        <div class="text-white absolute w-full hover:underline cursor-pointer top-0 ">
+          {{ artist.name }}
+        </div>
+        <div class="text-gray-300 text-[13px] flex">
+          <div class="flex">
+            Album
+          </div>
+          <div class="ml-2 flex">
+            <div class="w-[4px] h-[4px] bg-[#bdbdbd] rounded-full mt-2 mr-2" />
+            <span class="-ml-0.5">
+              {{ artist.releaseYear }} songs
+            </span>
+          </div>
+        </div>
+
+        <div class="absolute flex gap-4 items-center justify-start bottom-0 mb-1.5">
+          <button type="button" class="p-1 rounded-full bg-white">
+            <Play
+              v-if="true"
+              fillColor="#181818"
+              :size="25"
+            />
+            <Play
+              v-else
+              fillColor="#181818"
+              :size="25"
+            />
+          </button>
+          <button type="button">
+            <Heart
+              fillColor="#1BD760"
+              :size="30"
+            />
+          </button>
+          <button type="button">
+            <DotsHorizontal
+              fillColor="#FFFFFF"
+              :size="25"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="mt-6" />
+    <div class="flex items-center justify-between px-5 pt-2">
+      <div class="flex items-center justify-between text-gray-400">
+        <div class="mr-7">#</div>
+        <div class="text-sm">
+          Title
+        </div>
+      </div>
+      <div>
+        <ClockTimeThreeOutline
+          fillColor="#FFFFFF"
+          :size="18"
+        />
+      </div>
+    </div>
+    <div class="border-b border-b-[#2A2A2A] mt-2" />
+    <div class="mb-4" />
+    <ul
+      class="w-full"
+      v-for="track, index in artist.tracks"
+      :key="track"
+    >
+      <SongRow
+        :artist="artist"
+        :track="track"
+        :index="++index"
+      />
+    </ul>
   </div>
 </template>
